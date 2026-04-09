7ac657ca87bd563b2b67b4ca6006efb20c621e22
DJ <dj@walker.dev>
2026-04-09 19:36:25 +0000

    Bug 2000068 - enable opening tab note editor from tab preview. r=sthompson,desktop-theme-reviewers,tabbrowser-reviewers,fluent-reviewers,bolsson,dao
    Differential Revision: https://phabricator.services.mozilla.com/D279086

--- browser/components/tabbrowser/content/tabnote-menu.js
+++ browser/components/tabbrowser/content/tabnote-menu.js
@@ -31,16 +31,15 @@
         role="dialog"
         ignorekeys="true"
         norolluponanchor="true"
-        aria-labelledby="tab-note-editor-title"
         consumeoutsideclicks="false">
 
-        <html:div class="panel-header" >
+        <html:div class="panel-header" id="tab-note-editor-header" >
           <html:h1
             id="tab-note-editor-title">
           </html:h1>
         </html:div>
 
-        <toolbarseparator />
+        <toolbarseparator id="tab-note-editor-separator" />
 
         <html:div
           class="panel-subview-body
@@ -56,11 +55,15 @@
 
         <html:div
           class="panel-action-row panel-footer">
+          <html:moz-button
+              id="tab-note-editor-button-delete"
+              type="icon ghost"
+              data-l10n-id="tab-note-editor-button-delete">
+          </html:moz-button>
           <html:div
             id="tab-note-overflow-indicator">
           </html:div>
           <html:moz-button-group
-              class="tab-note-create-actions tab-note-create-mode-only"
               id="tab-note-default-actions">
               <html:moz-button
                   id="tab-note-editor-button-cancel"
@@ -80,13 +83,16 @@
     #initialized = false;
     #panel;
     #noteField;
+    #headerEl;
+    #separatorEl;
     #titleNode;
     /** @type {MozTabbrowserTab} */
     #currentTab = null;
-    /** @type {boolean} */
-    #createMode;
+    /** @type {boolean|null} */
+    #createMode = null;
     #cancelButton;
     #saveButton;
+    #deleteButton;
     #overflowIndicator;
     /** @type {TabNoteTelemetrySource|null} */
     #telemetrySource = null;
@@ -102,9 +108,12 @@
 
       this.#panel = this.querySelector("panel");
       this.#noteField = document.getElementById("tab-note-text");
-      this.#titleNode = document.getElementById("tab-note-editor-title");
+      this.#headerEl = this.querySelector("#tab-note-editor-header");
+      this.#separatorEl = this.querySelector("#tab-note-editor-separator");
+      this.#titleNode = this.querySelector("#tab-note-editor-title");
       this.#cancelButton = this.querySelector("#tab-note-editor-button-cancel");
       this.#saveButton = this.querySelector("#tab-note-editor-button-save");
+      this.#deleteButton = this.querySelector("#tab-note-editor-button-delete");
       this.#overflowIndicator = this.querySelector(
         "#tab-note-overflow-indicator"
       );
@@ -114,6 +123,9 @@
       });
       this.#saveButton.addEventListener("click", () => {
         this.saveNote();
+      });
+      this.#deleteButton.addEventListener("click", () => {
+        this.#deleteNote();
       });
       this.#panel.addEventListener("keypress", this);
       this.#panel.addEventListener("popuphidden", this);
@@ -163,6 +175,14 @@
         : "tab-note-editor-title-edit";
       this.#titleNode.innerText =
         gBrowser.tabLocalization.formatValueSync(headerL10nId);
+      this.#headerEl.hidden = !createModeEnabled;
+      this.#separatorEl.hidden = !createModeEnabled;
+      this.#deleteButton.hidden = createModeEnabled;
+      this.#panel.setAttribute(
+        "aria-label",
+        gBrowser.tabLocalization.formatValueSync(headerL10nId)
+      );
+
       this.#createMode = createModeEnabled;
     }
 
@@ -229,6 +249,10 @@
       if (!TabNotes.isEligible(tab)) {
         return;
       }
+      // Lazily set the icon to avoid loading it at startup
+      if (!this.#deleteButton.iconSrc) {
+        this.#deleteButton.iconSrc = "chrome://global/skin/icons/delete.svg";
+      }
       this.#currentTab = tab;
       this.#telemetrySource = options.telemetrySource;
 
@@ -270,6 +294,15 @@
         });
       }
 
+      this.#panel.hidePopup();
+    }
+
+    #deleteNote() {
+      if (TabNotes.isEligible(this.#currentTab)) {
+        TabNotes.delete(this.#currentTab, {
+          telemetrySource: this.#telemetrySource,
+        });
+      }
       this.#panel.hidePopup();
     }
   }
