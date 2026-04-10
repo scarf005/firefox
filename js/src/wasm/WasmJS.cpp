32da16028144e6b6966322d132a458f46677104e
agoloman <agoloman@mozilla.com>
2026-04-10 23:20:31 +0300

    Revert "Bug 2023217 - wasm: Add tests for JS-PI with stack switching. r=yury" for causing SM bustages @CodeGenerator-shared.h.
    This reverts commit 2cfab4fac2c82f9ce45220308e7ab493eb2fed3b.
    
    Revert "Bug 2023217 - wasm: Add stack-switching proposal and implement JS-PI with it. r=yury,jonco"
    
    This reverts commit fda0d9ad12966f6ef929a39a76700013f1c03233.
    
    Revert "Bug 2023217 - wasm: Add validation and decoding for stack-switching opcodes. r=yury"
    
    This reverts commit 0a7b2e940e968403d78325cb88502ec98fb00c0a.
    
    Revert "Bug 2023217 - wasm: Add stack switching type system. r=yury"
    
    This reverts commit 225be1cc8924b5cb91c527f118a6417d0bfc9d77.
    
    Revert "Bug 2023217 - wasm: Add stack switching preference and feature flag. r=yury"
    
    This reverts commit bd673e76f92b2e21ebb68c3e992ae8039785f959.
    
    Revert "Bug 2023217 - wasm: Add assert* and Printf utilities to MacroAssembler. r=yury"
    
    This reverts commit 77d31ad93381679f6c69fd4b4ab6599dc2dce0da.

--- js/src/wasm/WasmJS.cpp
+++ js/src/wasm/WasmJS.cpp
@@ -3739,7 +3739,7 @@ void WasmExceptionObject::trace(JSTracer* trc, JSObject* obj) {
 
   wasm::SharedTagType tag = exnObj.tagType();
   const wasm::ValTypeVector& params = tag->argTypes();
-  const wasm::TagOffsetVector& offsets = tag->exceptionArgOffsets();
+  const wasm::TagOffsetVector& offsets = tag->argOffsets();
   uint8_t* typedMem = exnObj.typedMem();
   for (size_t i = 0; i < params.length(); i++) {
     ValType paramType = params[i];
@@ -3844,7 +3844,7 @@ bool WasmExceptionObject::construct(JSContext* cx, unsigned argc, Value* vp) {
 
   wasm::SharedTagType tagType = exnObj->tagType();
   const wasm::ValTypeVector& params = tagType->argTypes();
-  const wasm::TagOffsetVector& offsets = tagType->exceptionArgOffsets();
+  const wasm::TagOffsetVector& offsets = tagType->argOffsets();
 
   RootedValue nextArg(cx);
   for (size_t i = 0; i < params.length(); i++) {
@@ -4015,7 +4015,7 @@ bool WasmExceptionObject::getArgImpl(JSContext* cx, const CallArgs& args) {
     return false;
   }
 
-  uint32_t offset = exnTag->tagType()->exceptionArgOffsets()[index];
+  uint32_t offset = exnTag->tagType()->argOffsets()[index];
   RootedValue result(cx);
   if (!exnObj->loadArg(cx, offset, params[index], &result)) {
     return false;
@@ -4307,7 +4307,7 @@ bool WasmFunctionConstruct(JSContext* cx, unsigned argc, Value* vp) {
 
   if (!IsCallableNonCCW(args[1])) {
     JS_ReportErrorNumberUTF8(cx, GetErrorMessage, nullptr,
-                             JSMSG_WASM_BAD_FUNCTION_VALUE, "second");
+                             JSMSG_WASM_BAD_FUNCTION_VALUE);
     return false;
   }
   RootedObject func(cx, &args[1].toObject());
@@ -5484,7 +5484,7 @@ bool WasmSuspendingObject::construct(JSContext* cx, unsigned argc, Value* vp) {
 
   if (!IsCallableNonCCW(args[0])) {
     JS_ReportErrorNumberUTF8(cx, GetErrorMessage, nullptr,
-                             JSMSG_WASM_BAD_FUNCTION_VALUE, "first");
+                             JSMSG_WASM_BAD_FUNCTION_VALUE);
     return false;
   }
 
@@ -5513,7 +5513,7 @@ static bool WebAssembly_promising(JSContext* cx, unsigned argc, Value* vp) {
 
   if (!IsWasmFunction(args[0])) {
     JS_ReportErrorNumberUTF8(cx, GetErrorMessage, nullptr,
-                             JSMSG_WASM_BAD_FUNCTION_VALUE, "first");
+                             JSMSG_WASM_BAD_FUNCTION_VALUE);
     return false;
   }
 
@@ -5689,15 +5689,6 @@ static bool WebAssemblyClassFinish(JSContext* cx, HandleObject object,
         return false;
       }
     }
-
-    SharedTagType jsPromiseTagType(sJSPromiseTagType);
-    WasmTagObject* jsPromiseTagObject =
-        WasmTagObject::create(cx, jsPromiseTagType, tagProto);
-    if (!jsPromiseTagObject) {
-      return false;
-    }
-
-    wasm->setJSPromiseTag(jsPromiseTagObject);
   }
 #endif
 
