66f216869d6293a483f5655352297b723928b0e3
Karl Tomlinson <karlt+@karlt.net>
2026-04-15 23:38:18 +0000

    Bug 2027433 Treat null input channels as zero in InterleaveAndConvertBuffer() r=padenot
    The loop nesting is inverted so as not to add a null check to the inner loop,
    but this change would also have some impact on cache use:
    
    * The loop ordering might perhaps not be the ideal for two channels and large
      sample counts, for example, because it means going through the
      output/largest buffer twice.
    
    * It does, however, avoid pathological situations of iterating across planar
      channel buffers, likely to load set-associative cache lines if the channel
      buffers are page aligned, due to having 1024 (or somewhat fewer) floats, for
      example.
    
    Differential Revision: https://phabricator.services.mozilla.com/D293825

--- dom/media/AudioSegment.h
+++ dom/media/AudioSegment.h
@@ -75,14 +75,21 @@ const uint32_t WEBAUDIO_BLOCK_SIZE = 1 << WEBAUDIO_BLOCK_SIZE_BITS;
 template <typename SrcT, typename DestT>
 static void InterleaveAndConvertBuffer(const SrcT* const* aSourceChannels,
                                        uint32_t aLength, float aVolume,
-                                       uint32_t aChannels, DestT* aOutput) {
-  DestT* output = aOutput;
-  for (size_t i = 0; i < aLength; ++i) {
-    for (size_t channel = 0; channel < aChannels; ++channel) {
-      float v =
-          ConvertAudioSample<float>(aSourceChannels[channel][i]) * aVolume;
-      *output = FloatToAudioSample<DestT>(v);
-      ++output;
+                                       uint32_t aChannelCount, DestT* aOutput) {
+  for (size_t channel = 0; channel < aChannelCount; ++channel) {
+    DestT* output = aOutput + channel;
+    if (aSourceChannels[channel]) {
+      for (size_t i = 0; i < aLength; ++i) {
+        float v =
+            ConvertAudioSample<float>(aSourceChannels[channel][i]) * aVolume;
+        *output = FloatToAudioSample<DestT>(v);
+        output += aChannelCount;
+      }
+    } else {
+      for (size_t i = 0; i < aLength; ++i) {
+        *output = static_cast<DestT>(0);
+        output += aChannelCount;
+      }
     }
   }
 }
