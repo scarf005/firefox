22b7e52eab8abe913c9a521e1922bce72171ee21
Henri Sivonen <hsivonen@hsivonen.fi>
2026-04-16 17:09:04 +0000

    Bug 2028443 - Avoid incorrect short time zone names for Europe/London in en-GB. r=dminor
    The previous state in Gecko was that GMT+1 was in some cases formatted as "GMT",
    which was a regression from a Web compat fix in bug 2018880.
    
    Also, GMT+2 was in some cases formatted as "BST", which is an upstream bug
    https://unicode-org.atlassian.net/browse/CLDR-19382 .
    
    The current upstream stance seems to be to stop doing localized time zone
    abbreviations before 1970-01-01, but that caused the Web compat bug 2018880.
    
    Differential Revision: https://phabricator.services.mozilla.com/D294097

--- js/src/tests/non262/Intl/DateTimeFormat/british.js
+++ js/src/tests/non262/Intl/DateTimeFormat/british.js
@@ -4,15 +4,32 @@
 // The winter time abbreviation for London should be
 // "GMT" in British English even prior to 1971-10-31.
 
-let parts = new Intl.DateTimeFormat("en-GB", {
-  timeZone: "Europe/London",
-  timeZoneName: "short"
-}).formatToParts(new Date("1968-01-01"));
+function formatLondon(dateAsString, locale) {
+  let parts = new Intl.DateTimeFormat(locale, {
+    timeZone: "Europe/London",
+    timeZoneName: "short"
+  }).formatToParts(new Date(dateAsString));
 
-let part = parts.find(p => p.type == "timeZoneName");
+  let part = parts.find(p => p.type == "timeZoneName");
 
-assertEq(part.type, "timeZoneName");
-assertEq(part.value, "GMT");
+  return part.value;
+}
+
+// Check offset assumptions by using en-US
+assertEq(formatLondon("1947-04-12", "en-US"), "GMT+1");
+assertEq(formatLondon("1947-04-14", "en-US"), "GMT+2");
+assertEq(formatLondon("1968-01-01", "en-US"), "GMT+0");
+assertEq(formatLondon("2026-01-01", "en-US"), "GMT"); // Metazones work from 1970 onwards!
+assertEq(formatLondon("2026-04-12", "en-US"), "GMT+1");
+assertEq(formatLondon("2026-04-14", "en-US"), "GMT+1");
+
+// Now check en-GB
+assertEq(formatLondon("1947-04-12", "en-GB"), "BST");
+assertEq(formatLondon("1947-04-14", "en-GB"), "GMT+2"); // Assuming BDST no longer recognized by users.
+assertEq(formatLondon("1968-01-01", "en-GB"), "GMT");
+assertEq(formatLondon("2026-01-01", "en-GB"), "GMT");
+assertEq(formatLondon("2026-04-12", "en-GB"), "BST");
+assertEq(formatLondon("2026-04-14", "en-GB"), "BST");
 
 if (typeof reportCompare === "function") {
     reportCompare(0, 0, "ok");
