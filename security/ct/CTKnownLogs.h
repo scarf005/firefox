ec1c164b7c46fa995af6084e2fd41629d74937bb
ffxbld <ffxbld@mozilla.com>
2026-04-16 20:32:14 +0000

    No Bug, mozilla-central repo-update remote-settings tld-suffixes mobile-experiments ct-logs - r=release-managers,RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D294756

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1782245158000000);
+static const PRTime kCTExpirationTime = INT64_C(1782418127000000);
 
 namespace mozilla::ct {
 
